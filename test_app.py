643eac473902aa2fd30f43f157ed313134801f44
abdelrahman-fadlallah <92474429+am8991857@users.noreply.github.com>
2026-05-19 17:25:08 +0300

    Update home page test in test_app.py
    Refactor test structure and add content assertion.

--- test_app.py
+++ test_app.py
@@ -1,11 +1,7 @@
-import pytest
 from app import app
 
-@pytest.fixture
-def client():
-    with app.test_client() as client:
-        yield client
-
-def test_home_page(client):
-    response = client.get('/')
+def test_home():
+    client = app.test_client()
+    response = client.get("/")
     assert response.status_code == 200
+    assert b"Hello from Jenkins CI/CD Pipeline!" in response.data
